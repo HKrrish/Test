1099d2e2f98b52e18f88fb3d7f6a1ee75c6bd12c
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2024-02-12 19:11:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -3051,9 +3051,9 @@ async def auto_filter(client, msg, spoll=False):
             user_id = message.from_user.id
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
 	    if settings["spell_check"]:
-                    return await advantage_spell_chok(msg)
-                else:
-                    return
+                return await advantage_spell_chok(msg)
+            else:
+                return
             #if not files:
             #    await client.send_message(req_channel,f"-🦋 #REQUESTED_CONTENT 🦋-\n\n📝**Content Name** :`{search}`\n**Requested By**: {message.from_user.first_name}\n **USER ID**:{user_id}\n\n🗃️",
                                                                                                        #reply_markup=InlineKeyboardMarkup([

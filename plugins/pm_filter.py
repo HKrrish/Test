1b2a306a0e2d1b662259702b66359ee2e072d823
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2024-02-12 14:36:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -79,12 +79,11 @@ BUTTONS2 = {}
 #         except Exception as e:
 #             logger.error(f"Chat Not verified : {e}") 
 
-@Client.on_message(filters.text & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
-    if message.from_user.id not in ADMINS:
-        k = await manual_filters(client, message)
-        if k == False:
-            await auto_filter(client, message)
+    k = await manual_filters(client, message)
+    if k == False:
+        await auto_filter(client, message)
 		
 @Client.on_callback_query(filters.regex('rename'))
 async def rename(bot,update):
